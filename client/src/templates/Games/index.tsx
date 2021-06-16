47d97ecc6d6a01869eefa3e4cfc45784cf34b75a
Lucas Siqueira <lucassiqzro@gmail.com>
2021-06-16 00:04:41 -0300

    Create useQueryGames

--- client/src/templates/Games/index.tsx
+++ client/src/templates/Games/index.tsx
@@ -1,10 +1,7 @@
-import { useQuery } from '@apollo/client'
+import { useQueryGames } from 'graphql/queries/games'
 
 import Base from 'templates/Base'
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
-
-import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
-import { QUERY_GAMES } from 'graphql/queries/games'
 
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
 import GameCard, { GameCardProps } from 'components/GameCard'
@@ -18,10 +15,9 @@ export type GamesTemplateProps = {
 }
 
 const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
-  const { data, loading, fetchMore } = useQuery<
-    QueryGames,
-    QueryGamesVariables
-  >(QUERY_GAMES, { variables: { limit: 15 } })
+  const { data, loading, fetchMore } = useQueryGames({
+    variables: { limit: 15 }
+  })
 
   const handleFilter = () => {
     return
