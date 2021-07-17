491ab372f61965a1585c49d0c180788be897a95f
Lucas Siqueira <lucassiqzro@gmail.com>
2021-07-17 11:04:16 -0300

    Hidden show more

--- client/src/templates/Games/test.tsx
+++ client/src/templates/Games/test.tsx
@@ -5,7 +5,7 @@ import { MockedProvider } from '@apollo/client/testing'
 import { renderWithTheme } from 'utils/tests/helpers'
 import apolloCache from 'utils/apolloCache'
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import { fetchMoreMock, gamesMock } from './mocks'
+import { fetchMoreMock, gamesMock, noGamesMock } from './mocks'
 
 import Games from '.'
 
@@ -52,7 +52,7 @@ describe('<Games />', () => {
 
   it('should render empty when no games found', async () => {
     renderWithTheme(
-      <MockedProvider mocks={[]} addTypename={false}>
+      <MockedProvider mocks={[noGamesMock]} addTypename={false}>
         <Games filterItems={filterItemsMock} />
       </MockedProvider>
     )
