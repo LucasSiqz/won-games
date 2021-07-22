6e7c8411d7a42a745b00a4c7538b74440c74a025
Lucas Siqueira <lucassiqzro@gmail.com>
2021-07-22 00:56:33 -0300

    Create Loader component

--- client/src/components/CartList/index.tsx
+++ client/src/components/CartList/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Button from 'components/Button'
 import GameItem from 'components/GameItem'
 import Empty from 'components/Empty'
+import Loader from 'components/Loader'
 
 import { useCart } from 'hooks/use-cart'
 
@@ -12,7 +13,15 @@ export type CartListProps = {
 }
 
 const CartList = ({ hasButton = false }: CartListProps) => {
-  const { items, total } = useCart()
+  const { items, total, loading } = useCart()
+
+  if (loading) {
+    return (
+      <S.Loading>
+        <Loader />
+      </S.Loading>
+    )
+  }
 
   return (
     <S.Wrapper isEmpty={!items.length}>
