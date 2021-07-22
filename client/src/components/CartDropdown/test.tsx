69ea74a0dea44ca570de9fa127107fb183d58832
Lucas Siqueira <lucassiqzro@gmail.com>
2021-07-21 22:41:42 -0300

    Adjust components to use cart context

--- client/src/components/CartDropdown/test.tsx
+++ client/src/components/CartDropdown/test.tsx
@@ -6,18 +6,23 @@ import items from 'components/CartList/mock'
 import CartDropdown from '.'
 
 describe('<CartDropdown />', () => {
-  it('should render <CartIcon /> and its badge', () => {
-    render(<CartDropdown items={items} total="R$ 300,00" />, {
-      cartProviderProps: { ...CartContextDefaultValues, quantity: items.length }
-    })
+  beforeEach(() => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      items,
+      quantity: items.length,
+      total: 'R$ 300,00'
+    }
 
+    render(<CartDropdown />, { cartProviderProps })
+  })
+
+  it('should render <CartIcon /> and its badge', () => {
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
     expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
   })
 
   it('should render Dropdown content with cart items and total', () => {
-    render(<CartDropdown items={items} total="R$ 300,00" />)
-
     expect(screen.getByText('R$ 300,00')).toBeInTheDocument()
     expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
   })
